ce5b9bdb841fea17e004e2a64da702cb378cd737
GustavoSantos <gsf34062@gmail.com>
2025-10-21 12:46:36 -0300

    Entity User e REST funcionando

---
+++ src/test/java/com/codefylab/mongodbspringboot/MongoDbSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefylab.mongodbspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
